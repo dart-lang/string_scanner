da81e56d49c2ff2e4e73e16de69c04ae29092fd1
Nate Bosch <nbosch@google.com>
2021-01-29 10:09:44 -0800

    Revert "Remove dependency on package:charcode (#29)"
    This reverts commit 7e41395d327e93b432f380aaf4c9668f0d952ef3.
    
    Since this has not been published or rolled through to the SDK or
    Flutter we will hold it back to reduce risk.

--- lib/src/eager_span_scanner.dart
+++ lib/src/eager_span_scanner.dart
@@ -2,7 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'charcode.dart';
+import 'package:charcode/ascii.dart';
+
 import 'line_scanner.dart';
 import 'span_scanner.dart';
 
--- test/line_scanner_test.dart
+++ test/line_scanner_test.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'package:string_scanner/src/charcode.dart';
+import 'package:charcode/charcode.dart';
 import 'package:string_scanner/string_scanner.dart';
 import 'package:test/test.dart';
 
