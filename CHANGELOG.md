17b2bc8426364e738951b527cfebbaa2c43ca39a
Nate Bosch <nbosch@google.com>
2021-02-04 16:17:00 -0800

    Revert "Revert "Remove dependency on package:charcode (#29)" (#33)" (#36)
    This reverts commit 1719b933f1e46b302f0170df205144831f35958d.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,5 @@
+## 1.1.1-dev
+
 ## 1.1.0
 
 * Stable release for null safety.
--- lib/src/eager_span_scanner.dart
+++ lib/src/eager_span_scanner.dart
@@ -2,8 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'package:charcode/ascii.dart';
-
+import 'charcode.dart';
 import 'line_scanner.dart';
 import 'span_scanner.dart';
 
